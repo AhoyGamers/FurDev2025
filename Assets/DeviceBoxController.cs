37c300ae6ebe85b53e4616d8a1937582f03afb15
Jakeb89 <1346278+Jakeb89@users.noreply.github.com>
2025-01-17 18:58:31 -0600

    Initial Commit

---
+++ Assets/DeviceBoxController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceBoxController : MonoBehaviour{
+    [SerializeField] Animator animator;
+    [SerializeField] bool selected;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    bool IsInTransition(){
+        return (animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+    }
+
+    public bool Open(){
+        animator.SetBool("BoolOpen", true);
+        return true;
+    }
+    public bool Close(){
+        animator.SetBool("BoolOpen", false);
+        return true;
+    }
+
+    public void SetSelected(bool selected){
+        this.selected = selected;
+        if(selected) Open();
+        else Close();
+    }
+}
---
+++ Assets/ObjectClicker.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class ObjectClicker : MonoBehaviour{
+    static GameObject hoveredObject;
+    static GameObject selectedObject;
+
+    [SerializeField] GameObject debugHoveredObject;
+    [SerializeField] GameObject debugSelectedObject;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+        float nearestDistance = 0f;
+        GameObject nearestObject = null;
+        foreach(RaycastHit hit in hits){
+            GameObject raycastObject = hit.transform.gameObject;
+            print(raycastObject.name);
+            GameObject selectableGameObject = getAncestorWithTag(raycastObject, "Selectable");
+            if(selectableGameObject){
+                float raycastDistance = (selectableGameObject.transform.position - Camera.main.transform.position).magnitude;
+                if(nearestObject == null || raycastDistance < nearestDistance){
+                    nearestDistance = raycastDistance;
+                    nearestObject = selectableGameObject;
+                }
+            }
+        }
+        if(nearestObject){
+            setHoveredObject(nearestObject);
+            debugHoveredObject = nearestObject;
+        }else{
+            setHoveredObject(null);
+            debugHoveredObject = null;
+        }
+
+        if(Input.GetMouseButtonDown((int)MouseButton.Left)){
+            setSelectedObject(hoveredObject);
+            debugSelectedObject = hoveredObject;
+        }else if(Input.GetMouseButtonDown((int)MouseButton.Right)){
+            setSelectedObject(null);
+            debugSelectedObject = null;
+        }
+    }
+
+    GameObject getAncestorWithTag(GameObject gameObject, string tag){
+        if(!gameObject) return null;
+        if(gameObject.CompareTag(tag)) return gameObject;
+        if(!(gameObject.transform.parent)) return null;
+        return getAncestorWithTag(gameObject.transform.parent.gameObject, tag);
+    }
+
+    void setHoveredObject(GameObject gameObject){
+        if(hoveredObject){
+            Outline selectOutline = hoveredObject.GetComponent<Outline>();
+            if(selectOutline){
+                //selectOutline.enabled = false;
+            }
+        }
+        hoveredObject = gameObject;
+        if(hoveredObject){
+            Outline selectOutline = hoveredObject.GetComponent<Outline>();
+            if(selectOutline){
+                //selectOutline.enabled = true;
+            }
+        }
+    }
+
+    void setSelectedObject(GameObject gameObject){
+        if(selectedObject){
+            selectedObject.SendMessage("SetSelected", false);
+            /*
+            Outline selectOutline = selectedObject.GetComponentInChildren<Outline>();
+            if(selectOutline){
+                selectOutline.enabled = false;
+            }
+            */
+        }
+        selectedObject = gameObject;
+        if(selectedObject){
+            selectedObject.SendMessage("SetSelected", true);
+            /*
+            Outline selectOutline = selectedObject.GetComponentInChildren<Outline>();
+            if(selectOutline){
+                selectOutline.enabled = true;
+            }
+            */
+        }
+    }
+}
