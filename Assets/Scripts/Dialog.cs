1f4a627d15fe16ebf32ab043a76e35f5c4cfcf63
Xilats <xilats.dert@gmail.com>
2025-01-19 00:11:18 -0700

    Added Rudimentary dialog system

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    public string speakerName;
+    public string[] dialogs;
+}
