9cfe74c7e84cc2d446d48bdb37e1458284e81287
Ardolf <96351049+Awrdolf@users.noreply.github.com>
2025-01-26 18:10:58 +0800

    Added FMOD into the project
    - Added one test BGM
    - Added AudioManager with various methods
    (PlayBGM, StopBGM, PlaySFX)

--- Assets/Scripts/IntroUI.cs
+++ Assets/Scripts/IntroUI.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using FMOD.Studio;
 
 public class IntroUI : MonoBehaviour
 {
     //references we need
     [SerializeField] Button startButton;
     [SerializeField] Button quitButton;
-
     private void Start()
     {
         startButton.onClick.AddListener(startGame);
         quitButton.onClick.AddListener(quitGame);
 
+        AudioManager.StartBGM("test_bgm");
     }
 
     private void startGame()
     {
         LoadFirstLevel();
+        AudioManager.StopBGM();
     }
 
     private void quitGame()
@@ -31,5 +33,4 @@ public class IntroUI : MonoBehaviour
         int nextScene = 1;
         SceneManager.LoadScene(nextScene);
     }
-
 }
